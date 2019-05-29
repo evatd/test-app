c0e333410eda631d0b130e998b520d4ea300b631
Eva Tkautz <eva.tkavc@gmail.com>
2019-05-29 12:38:22 +0200

    Call the AC from the component - vs dispatch

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
 // to create an instance of connect component
 import { connect } from "react-redux";
+// importing a plain old JS function, 
+// it gets called and we return an object
+import { selectSong } from "./../actions";
 
 class SongList extends Component {
+  renderList = () =>
+    this.props.songs.map(song => (
+      <div key={song.title}>
+        <p>{song.title}</p>
+        {/* call the AC and pass down the song we're currently iterating over */}
+        <button onClick={() => this.props.selectSong(song)}>
+          {"Select Song"}
+        </button>
+      </div>
+    ));
   render() {
-    return this.props.songs.map((song) => <div key={song.title}>{song.title}</div>)
+    return <div>{this.renderList()}</div>;
   }
 }
 
 // get my state
 const mapStateToProps = state => {
+  console.log("state", state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+// selectSong: selectSong
+// connect will now pass this AC to our comp
+// as a prop
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);

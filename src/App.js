03aec4b5afdca6b8a06c9883858eb01b41da0fa3
Eva Tkautz <eva.tkavc@gmail.com>
2019-06-06 14:00:15 +0100

    App set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import UserCreate from "./UserCreate";
 
-const App = () => {
-  return <div style={{ margin: 0, padding: 0 }}>{"hi"}</div>;
-};
+class App extends Component {
+  state = { language: "english" };
+
+  onLangugeChange = language => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div style={{ margin: 0, padding: 0 }}>
+        <button onClick={() => this.onLangugeChange("english")}>English</button>
+        <button onClick={() => this.onLangugeChange("dutch")}>Dutch</button>
+        {this.state.language}
+        <UserCreate />
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Field.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Field extends Component {
+  render() {
+    return (
+      <div>
+        <label>Name</label>
+        <input />
+      </div>
+    );
+  }
+}
+export default Field;

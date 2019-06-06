ee3dc78806d18a27f9bf8e2de4450b935055373e
Eva Tkautz <eva.tkavc@gmail.com>
2019-06-06 19:39:50 +0100

    Create a second context - would need emotion for color to show up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 class App extends Component {
   state = { language: "english" };
 
@@ -13,9 +14,12 @@ class App extends Component {
         <button onClick={() => this.onLangugeChange("english")}>English</button>
         <button onClick={() => this.onLangugeChange("dutch")}>Dutch</button>
         {this.state.language}
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        {/* hardcoded value for illustration: wrapping order doesnt matter */}
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
-// On the receiving / component end
+// On the receiving / component end: see how you wrapped its parent
+// i.e.  UserCreate in the context, and we can access this data in Button
+// without passing it through the latter first
 class Button extends Component {
   // not needed when using Provider - only to access data via this.context
   // static contextType = LanguageContext
 
   renderSubmit = value => (value === "english" ? "Submit" : "Voorleegen");
   render() {
-    // we're providing a function as a child to the component
+    // We're providing a function as a child to the component
     // this child is called with whatever value is in the pipe (Consumer) -
-    // see value in Provider: value={this.state.language}
+    // See value in Provider: value={this.state.language}
     // wrap this consumer logic in the desired HTML element = button
+    // Note how you can call this prop color if you want - or value - up to you
     return (
-      <button>
-        <LanguageContext.Consumer>
-          {value => this.renderSubmit(value)}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {color => (
+          <button
+            css={`
+              color: ${color};
+            `}
+          >
+            <LanguageContext.Consumer>
+              {value => this.renderSubmit(value)}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     );
   }
 }

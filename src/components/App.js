0531f5967bee977d24f2152f358fdceaea4c904d
Eva Tkautz <eva.tkavc@gmail.com>
2019-06-08 17:18:39 +0100

    Access props from store in App and prep for dev tools

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const App = () => {
-  return <div style={{ margin: 0, padding: 0 }}>{"hi"}</div>;
+const App = props => {
+  console.log(props.lists);
+  return (
+    <div style={{ margin: 0, padding: 0 }}>
+      <ul>
+        {props.lists.map((list, id) => (
+          <li key={id}>{list.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
-  console.log(state)
-  return state
-}
+  console.log(state);
+  return { lists: state.lists };
+};
 export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import App from "./components/App";
-import reducers from "./reducers"
+import reducers from "./reducers";
 import * as serviceWorker from "./serviceWorker";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, composeEnhancers())}>
     <App />
   </Provider>,
   document.getElementById("root")
